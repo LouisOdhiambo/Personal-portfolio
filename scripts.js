aebb70d0bbd41cf678c2876f1cd419ec3bb667fa
Louis-crypto <kinglouis745@gmail.com>
2020-04-23 13:45:24 +0300

    add jquery onclick function to go back up when up icon is clicked

--- scripts.js
+++ scripts.js
@@ -3,4 +3,27 @@ $(document).ready(function (){
       $(this).toggleClass('open');
       $('.navBar').toggleClass('open');
     });
+
+    $('.navBar').on ('click', function (){
+      $('.menuTogler').removeClass('open');
+      $('.navBar').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on ('click', function (){
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 100
+      }, 2000);
+    });
+
+    $('#up').on ('click', function (){
+      $('html, body').animate({
+        scrollTop: 0
+      }, 2000);
+    });
+
+    AOS.init({
+      easing:'ease',
+      duration: 1800
+    });
+
 });
