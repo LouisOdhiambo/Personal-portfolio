6fcde005f6e68fa7e3bf13a3b95cfb8f0d0efe1f
Louis-crypto <kinglouis745@gmail.com>
2022-05-11 19:10:37 +0300

    add typewriter effect

--- scripts.js
+++ scripts.js
@@ -28,4 +28,18 @@ $(document).ready(function (){
     });
 
 
+    let i = 0;
+    let target = document.getElementById("typewriter");
+    let text = target.innerHTML;
+    target.innerHTML = ' ';
+    let speed = 75; //speed duration of effect in millisec
+
+    typeWriter(); //to call function
+    function typeWriter() {
+        if (i < text.length) {
+            target.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
 });
